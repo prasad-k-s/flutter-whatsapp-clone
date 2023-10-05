496882d3cfe1ccb1d65e8763e543501e0d124d4c
Prasad K S <prasad07062001@gmail.com>
2023-10-05 12:33:32 +0530

    add firebase setup files

--- lib/info.dart
+++ lib/info.dart
@@ -3,15 +3,13 @@ const info = [
     'name': 'Rivaan Ranawat',
     'message': 'Hey, how are you doing?',
     'time': '3:53 pm',
-    'profilePic':
-        'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+    'profilePic': 'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
   },
   {
     'name': 'John Doe',
     'message': 'Hello, whats up',
     'time': '2:25 pm',
-    'profilePic':
-        'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
+    'profilePic': 'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
   },
   {
     'name': 'Naman Ranawat',
@@ -25,14 +23,13 @@ const info = [
     'message': 'Call me, have some work',
     'time': '12:06 pm',
     'profilePic':
-        'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
+        'https://images.unsplash.com/photo-1691036561573-4b76998b60de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80',
   },
   {
     'name': 'Mom',
     'message': 'You ate food?',
     'time': '10:00 am',
-    'profilePic':
-        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+    'profilePic': 'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
   },
   {
     'name': 'Jurica',
@@ -75,22 +72,10 @@ const messages = [
   {"isMe": false, "text": "Hey What is up with you!!", "time": "10:00 am"},
   {"isMe": true, "text": "im fine,wbu?", "time": "11:00 am"},
   {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
-  {
-    "isMe": false,
-    "text": "Just messaged cuz I had some work.",
-    "time": "11:01 am"
-  },
+  {"isMe": false, "text": "Just messaged cuz I had some work.", "time": "11:01 am"},
   {"isMe": true, "text": "Obviously, say", "time": "11:03 am"},
-  {
-    "isMe": false,
-    "text": "haha I wanted you to check out my new channel ^^",
-    "time": "11:04 am"
-  },
-  {
-    "isMe": true,
-    "text": " Sure, what is the channel name?",
-    "time": "11:05 am"
-  },
+  {"isMe": false, "text": "haha I wanted you to check out my new channel ^^", "time": "11:04 am"},
+  {"isMe": true, "text": " Sure, what is the channel name?", "time": "11:05 am"},
   {
     "isMe": false,
     "text": "Rivaan Ranawat",
@@ -101,19 +86,9 @@ const messages = [
     "text": "Looks great to me!",
     "time": "11:15 am",
   },
-  
-
   {"isMe": false, "text": "Thanks bro!", "time": "11:17 am"},
-  {
-    "isMe": false,
-    "text": "Did you subscribe?",
-    "time": "11:16 am"
-  },
-  {
-    "isMe": true,
-    "text": "Yes, surely bro!",
-    "time": "11:17 am"
-  },
+  {"isMe": false, "text": "Did you subscribe?", "time": "11:16 am"},
+  {"isMe": true, "text": "Yes, surely bro!", "time": "11:17 am"},
   {
     "isMe": false,
     "text": "Cool, did you like the content?",
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_whatsapp_clone/colors.dart';
+import 'package:flutter_whatsapp_clone/firebase_options.dart';
 import 'package:flutter_whatsapp_clone/screens/mobile_layout_screen.dart';
 import 'package:flutter_whatsapp_clone/screens/web_layout_screen.dart';
 import 'package:flutter_whatsapp_clone/utils/responsive_layout.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
