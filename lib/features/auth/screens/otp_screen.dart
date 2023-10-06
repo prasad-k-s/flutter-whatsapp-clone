cd07331b4a4b1ae51becf868ffa56b6f47e13e0f
Prasad K S <prasad07062001@gmail.com>
2023-10-06 11:03:53 +0530

    add OTP screen

--- lib/features/auth/screens/otp_screen.dart
+++ lib/features/auth/screens/otp_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:flutter_whatsapp_clone/colors.dart';
 
 class OTPScreen extends StatefulWidget {
   static const String routeName = '/otp-screen';
@@ -12,6 +14,35 @@ class OTPScreen extends StatefulWidget {
 class _OTPScreenState extends State<OTPScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        centerTitle: true,
+        title: const Text(
+          'Verify your number',
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            const Text('We have sent an SMS with a code'),
+            OtpTextField(
+              margin: const EdgeInsets.all(8),
+              numberOfFields: 6,
+              fieldWidth: 40,
+              keyboardType: TextInputType.number,
+              showFieldAsBox: false,
+              focusedBorderColor: tabColor,
+              mainAxisAlignment: MainAxisAlignment.center,
+              onSubmit: (value) {},
+              autoFocus: true,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
